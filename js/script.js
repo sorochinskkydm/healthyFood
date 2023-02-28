61ab489fa53b214807de43a3384969a27a679854
sorochinskkydm <sorochinskkydm@gmail.com>
2023-02-28 18:43:14 +0300

    Создание карточек меню с помощью класса

--- js/script.js
+++ js/script.js
@@ -38,6 +38,8 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    //timer
+
     const deadline = "2023-09-22";
         function getTimeRemaining(endtime){
             const t = Date.parse(endtime) - Date.parse(new Date());
@@ -94,9 +96,44 @@ document.addEventListener("DOMContentLoaded", () => {
             document.body.style.overflow = "";
         });
 
+        //cards
 
+        const containerCard = document.querySelector(".container"); 
+        class MenuCard {
+            constructor(img, subtitle, description, price) {
+                this.img = img;
+                this.subtitle = subtitle;
+                this. description = description;
+                this. price = price;
+            }
 
+            createMenuCard = function() {
+                const menuCardItem = document.createElement("div");
+                menuCardItem.innerHTML = 
+                                    `<div class="menu__item">
+                                        <img src="${this.img}" alt="vegy">
+                                        <h3 class="menu__item-subtitle">${this.subtitle}</h3>
+                                        <div class="menu__item-descr">${this.description}</div>
+                                        <div class="menu__item-divider"></div>
+                                        <div class="menu__item-price">
+                                            <div class="menu__item-cost">Цена:</div>
+                                            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                                        </div>
+                                    </div>`
+                containerCard.append(menuCardItem);
+            }
+        }
 
+        containerCard.innerHTML = "";
+
+        const menuCardItem1 = new MenuCard("img/tabs/vegy.jpg",'Меню "Фитнес"', "Меню 'Фитнес' - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!", 229);
+        menuCardItem1.createMenuCard();
+        const menuCardItem2 = new MenuCard("img/tabs/elite.jpg", 'Меню “Премиум”','В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 550);
+        menuCardItem2.createMenuCard();
+        const menuCardItem3 = new MenuCard("img/tabs/post.jpg", 'Меню "Постное"', 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.', 430);
+        menuCardItem3.createMenuCard();
+
+        
 
 
 
