f2e3617fae229c5f02a44525a13bbfe6a4386673
sorochinskkydm <sorochinskkydm@gmail.com>
2023-02-28 04:40:10 +0300

    Работа с датами, реализация обратного отсчета

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+"use strict";
+
+
 document.addEventListener("DOMContentLoaded", () => {
 
     const tabs = document.querySelectorAll(".tabheader__item");
@@ -34,6 +37,46 @@ document.addEventListener("DOMContentLoaded", () => {
             }); 
         }
     });
+
+    const deadline = "2023-09-22";
+        function getTimeRemaining(endtime){
+            const t = Date.parse(endtime) - Date.parse(new Date());
+            let days = Math.floor(t/(1000 * 60 * 60 * 24));
+            let hours = Math.floor((t/(1000 * 60 * 60)) % 24);
+            let minutes = Math.floor((t/1000/60) % 60);
+            let seconds = Math.floor((t/1000) % 60);
+
+            return {
+                "total": t,
+                "days": days,
+                "hours": hours,
+                "minutes": minutes,
+                "seconds": seconds
+            };
+        }
+
+        function setClock(selector, endtime) {
+            const timer = document.querySelector(selector)
+            let days = timer.querySelector("#days");
+            let hours = timer.querySelector("#hours");
+            let minutes = timer.querySelector("#minutes");
+            let seconds = timer.querySelector("#seconds");
+            let timeInterval = setInterval(updateClock, 1000);
+
+            function updateClock() {
+                const t = getTimeRemaining(endtime);
+                days.innerHTML = t.days;
+                hours.innerHTML = t.hours;
+                minutes.innerHTML = t.minutes;
+                seconds.innerHTML = t.seconds;
+
+                if(t.total <= 0) {
+                    clearInterval(timeInterval);
+                }
+            }
+        }
+
+        setClock(".timer", deadline);
 
 
 
