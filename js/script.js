ae6deea2bc8c20de0500fe335d7c11f74fa30074
sorochinskkydm <sorochinskkydm@gmail.com>
2023-03-03 21:58:53 +0300

    Работа с XMLHttpRequest

--- js/script.js
+++ js/script.js
@@ -158,6 +158,56 @@ document.addEventListener("DOMContentLoaded", () => {
             21,
             ".menu .container"
         ).render();
+
+        const forms = document.querySelectorAll("form");
+
+        const message = {
+            loading: "Загрузка",
+            success: "Спасибо, скоро мы с Вами свяжемся!",
+            failure: "Что-то пошло не так.."
+        }
+
+        function postData(form) {
+            form.addEventListener("submit", (event) => {
+                event.preventDefault();
+
+                const statusMessage = document.createElement("div");
+                statusMessage.classList.add("status");
+                statusMessage.textContent = message.loading;
+                form.append(statusMessage);
+
+                const request = new XMLHttpRequest();
+                request.open("POST", "server.php");
+                request.setRequestHeader("Content-type", "application/json");
+
+                const formData = new FormData(form);
+                const jsonObject = {};
+                formData.forEach((value,key) => {
+                    jsonObject[key] = value;
+                });
+
+                const json = JSON.stringify(jsonObject);
+
+                request.send(json);
+
+                request.addEventListener("load", () => {
+                    if(request.status === 200) {
+                        console.log(request.response);
+                        statusMessage.textContent = message.success;
+                        form.reset();
+                        setTimeout(function() {
+                            statusMessage.remove();
+                        }, 3000);
+                    } else {
+                        statusMessage.textContent = message.failure;
+                    }
+                });
+            });
+        }
+
+        forms.forEach(item => {
+            postData(item); 
+        });
         
 
 
