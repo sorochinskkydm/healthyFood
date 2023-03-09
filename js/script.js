785a03f45a7fa4492f640a017f4114c614635c72
sorochinskkydm <sorochinskkydm@gmail.com>
2023-03-09 05:58:49 +0300

    Работа со слайдером

--- js/script.js
+++ js/script.js
@@ -197,12 +197,53 @@ document.addEventListener("DOMContentLoaded", () => {
             bindPostData(item);
         });
 
-        fetch("http://localhost:3000/menu")
-        .then(data => data.json())
-        .then(res => console.log(res))
 
+
+
+        //slider
+
+        const slides = document.querySelectorAll(".offer__slide");
+        const previousBtn = document.querySelector(".offer__slider-prev");
+        const nextBtn = document.querySelector(".offer__slider-next");
+        const currentSlide = document.getElementById("current");
+        const totalSlides = document.getElementById("total");
+        let slideIndex = 1;
+        showSlides(slideIndex);
         
+        function showSlides(n) {
+            if(n > slides.length) {
+                slideIndex = 1;
+            }
+            if (n < 1) {
+                slideIndex = slides.length;
+            }
 
+            slides.forEach(item => {
+                item.style.display = "none";
+            });
+
+            slides[slideIndex - 1].style.display = "block";
+
+            if (slides.length < 10) {
+                totalSlides.textContent = "0" + slides.length;
+            } else totalSlides.textContent = slides.length;
+
+            if (slides.length < 10) {
+                currentSlide.textContent = "0" + slideIndex;
+            }
+        }
+
+        function plusSlides(n) {
+            showSlides(slideIndex += n);
+        }
+
+        previousBtn.addEventListener('click', () => {
+            plusSlides(-1);
+        });
+
+        nextBtn.addEventListener("click", () => {
+            plusSlides(1);
+        });
 
 
 
